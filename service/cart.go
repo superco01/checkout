cd0058f5dc718c4aa9e31bd21ea44dbb69f153cd
superco <habibyafi45@gmail.com>
2022-05-03 18:59:11 +0700

    add cart, product and promotion

---
+++ service/cart.go
@@ -0,0 +1,77 @@
+package service
+
+import (
+	"fmt"
+	"log"
+)
+
+type Cart struct {
+	Items map[string]int
+}
+
+func InitCart() *Cart {
+	return &Cart{
+		Items: make(map[string]int),
+	}
+}
+
+func (cart Cart) AddItem(key string) error {
+	product, found := products.ProductList[key]
+
+	if !found {
+		return fmt.Errorf("product with SKU number %v not found", key)
+	}
+
+	if product.Qty < 1 {
+		return fmt.Errorf("product %v with SKU number %v is out of stock", product.Name, key)
+	}
+	//update product stock in db
+	product.Qty -= 1
+	products.ProductList[key] = product
+
+	//update product item in cart
+	item, found := cart.Items[key]
+	if found {
+		cart.Items[key] = item + 1
+	} else {
+		cart.Items[key] = 1
+	}
+	log.Printf("cart2: %v", cart)
+
+	return nil
+}
+
+func (cart Cart) Checkout() (float32, error) {
+	fmt.Println("=====Checkout=====")
+	var total float32
+	log.Println(products.ProductList)
+	log.Println(cart)
+
+	for key, v := range cart.Items {
+		product := products.ProductList[key]
+		total = total + (product.Price * float32(v))
+	}
+
+	fmt.Printf("Total price: \t\t $%.2f \n", total)
+
+	discountPrice, promoDesc := cart.ApplyPromo()
+	cart.printItems()
+	fmt.Println("Promo description: \n" + promoDesc)
+	fmt.Printf("Total discount: \t\t $%.2f \n", discountPrice)
+
+	fmt.Printf("Total price after discount: \t $%.2f \n", (total - discountPrice))
+	log.Println("Total")
+	log.Println(cart)
+
+	return total, nil
+}
+
+func (cart Cart) printItems() {
+	for key, v := range cart.Items {
+		product, found := products.ProductList[key]
+		if found {
+			fmt.Printf("%v \t %vx \t $%.2f \n", product.Name, v, product.Price)
+		}
+	}
+
+}
---
+++ service/promotion.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"fmt"
+	"log"
+)
+
+func (cart Cart) ApplyPromo() (float32, string) {
+	var totalDiscountPrice float32
+	var promoDescription string
+
+	log.Printf("cart: %v", cart)
+
+	discountPrice, desc := cart.getBonusItem()
+	if desc != "" {
+		totalDiscountPrice += discountPrice
+		promoDescription += desc + "\n"
+	}
+
+	discountPrice, desc = cart.getDiscount()
+	if desc != "" {
+		totalDiscountPrice += discountPrice
+		promoDescription += desc + "\n"
+	}
+
+	discountPrice, desc = cart.payLessItem()
+	if desc != "" {
+		totalDiscountPrice += discountPrice
+		promoDescription += desc + "\n"
+	}
+
+	return totalDiscountPrice, promoDescription
+}
+
+func (cart Cart) getDiscount() (float32, string) {
+
+	//discount promo rule
+	discountedItem := "A304SD"
+	minPurchasedItem := 3
+	var discount float32 = 0.1
+
+	// promo desc
+	desc := fmt.Sprintf("Buying more than %v %vs will have a %v %% discount on all %vs", minPurchasedItem, products.ProductList[discountedItem].Name, (discount * 100), products.ProductList[discountedItem].Name)
+
+	item := cart.Items[discountedItem]
+
+	if item >= minPurchasedItem {
+		return products.ProductList[discountedItem].Price * float32(item) * float32(discount), desc
+	}
+	return 0, ""
+}
+
+func (cart Cart) getBonusItem() (float32, string) {
+
+	// bonus item promo rule
+	baseItem := "43N23P"
+	bonusItem := "234234"
+
+	// promo desc
+	desc := fmt.Sprintf("Each sale of a %v comes with a free %v", products.ProductList[baseItem].Name, products.ProductList[bonusItem].Name)
+
+	item := cart.Items[baseItem]
+
+	if item > 0 {
+		for i := 0; i < item; i++ {
+			cart.AddItem(bonusItem)
+		}
+		return products.ProductList["234234"].Price * float32(item), desc
+	}
+	return 0, ""
+}
+
+func (cart Cart) payLessItem() (float32, string) {
+
+	// pay less item promo rule
+	promoItem := "120P90"
+	minPurchasedItem := 3
+	paidItem := 2
+
+	// promo desc
+	desc := fmt.Sprintf("Buy %v %vs for the price of %v", minPurchasedItem, products.ProductList[promoItem].Name, paidItem)
+
+	item := cart.Items[promoItem]
+
+	if item >= minPurchasedItem {
+		unpaidItem := item / minPurchasedItem
+		return products.ProductList[promoItem].Price * float32(unpaidItem), desc
+	}
+	return 0, ""
+}
